c0ccd764219e33fde2f0ae6fcb1963dac6f4dabe
Denis Homolík <alfonz@homolik.cz>
2026-04-19 03:52:05 +0200

    add header columns

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -28,21 +28,41 @@ export const Table = () => {
           gridTemplateRows: gridTemplateRows.map((c) => `${c}px`).join(" "),
         }}
       >
+        {[...Array(state.width - 1)].map((_, i) => (
+          <div
+            className="jed-cell text-gray-400 text-center border-gray-400! text-sm"
+            style={{ gridColumnStart: i + 2, gridRowStart: 1 }}
+          >
+            {String.fromCharCode("A".charCodeAt(0) + (i % 26))}
+          </div>
+        ))}
         {state.data.map((s, i) => {
           const x = Math.round(
             (i / state.width - Math.floor(i / state.width)) * state.width,
           );
           const y = Math.floor(i / state.width);
 
+          console.log(x);
+
           return (
-            <div
-              className={`jed-cell ${focused?.offset === i ? "outline-2 outline-emerald-800" : ""}`}
-              key={`${x};${y}`}
-              style={{ gridColumnStart: x + 1, gridRowStart: y + 1 }}
-              onClick={() => setFocused(i)}
-            >
-              {s}
-            </div>
+            <>
+              {x === 0 && (
+                <div
+                  className="jed-cell text-gray-400 text-center border-gray-400! text-sm"
+                  style={{ gridColumnStart: x + 1, gridRowStart: y + 2 }}
+                >
+                  {y}
+                </div>
+              )}
+              <div
+                className={`jed-cell ${focused?.offset === i ? "outline-2 outline-emerald-800" : ""}`}
+                key={`${x};${y}`}
+                style={{ gridColumnStart: x + 2, gridRowStart: y + 2 }}
+                onClick={() => setFocused(i)}
+              >
+                {s}
+              </div>
+            </>
           );
         })}
       </div>
